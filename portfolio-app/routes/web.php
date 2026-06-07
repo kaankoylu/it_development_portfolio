65d99fc5ac0244683004ace04fc01b2ab4dfea3e
kaankoylu <ekankoylu@outlook.com>
2026-06-07 16:08:11 +0200

    web_php_update

--- portfolio-app/routes/web.php
+++ portfolio-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Public Showcase View
+Route::get('/', [PortfolioController::class, 'index'])->name('portfolio.index');
+
+//  Owner/Admin Workspace (Requires authentication login therefore seperate routing then other routes)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/owner/dashboard', [PortfolioController::class, 'dashboard'])->name('owner.dashboard');
+    Route::post('/owner/bio', [PortfolioController::class, 'updateBio'])->name('owner.bio.update');
+    Route::post('/owner/posts', [PortfolioController::class, 'storePost'])->name('owner.post.store');
+    Route::patch('/owner/posts/{post}', [PortfolioController::class, 'updatePostStatus'])->name('owner.post.update');
+    Route::post('/owner/courses', [PortfolioController::class, 'storeCourse'])->name('owner.course.store');
 });
 
 require __DIR__.'/auth.php';
